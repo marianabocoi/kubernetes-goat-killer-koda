89c8c0a029157a18ee96d337dcf4bad49fd54a99
Mariana B. (she/her/they/them) <10142989+marianabocoi@users.noreply.github.com>
2022-10-31 23:04:07 +0100

    pretty

--- scenario01/background_setup.sh
+++ scenario01/background_setup.sh
@@ -4,6 +4,29 @@ git clone https://github.com/madhuakula/kubernetes-goat.git
 
 kubectl apply -f kubernetes-goat/scenarios/build-code/deployment.yaml
 sleep 15
-export POD_NAME=$(kubectl get pods --namespace default -l "app=build-code" -o jsonpath="{.items[0].metadata.name}")
-kubectl port-forward $POD_NAME --address 0.0.0.0 1230:3000 > /dev/null 2>&1 &
+
+echo "  _  __     _                          _             " >kubegoat
+echo " | |/ /    | |                        | |            " >>kubegoat
+echo " | ' /_   _| |__   ___ _ __ _ __   ___| |_ ___  ___  " >>kubegoat
+echo " |  <| | | | '_ \ / _ \ '__| '_ \ / _ \ __/ _ \/ __| " >>kubegoat
+echo " | . \ |_| | |_) |  __/ |  | | | |  __/ ||  __/\__ \ " >>kubegoat
+echo " |_|\_\__,_|_.__/ \___|_|  |_| |_|\___|\__\___||___/ " >>kubegoat
+echo "                   | |                               " >>kubegoat
+echo "   __ _  ___   __ _| |_                              " >>kubegoat
+echo "  / _` |/ _ \ / _` | __|                             " >>kubegoat
+echo " | (_| | (_) | (_| | |_                              " >>kubegoat
+echo "  \__, |\___/ \__,_|\__|                             " >>kubegoat
+echo "   __/ |                                             " >>kubegoat
+echo "  |___/  " >>kubegoat
+
+
+echo 'echo waiting for init-background-script to finish'> startup.sh
+echo 'while [ ! -f /tmp/background0 ]; do sleep 1; done'>> startup.sh
+echo 'export POD_NAME=$(kubectl get pods --namespace default -l "app=build-code" -o jsonpath="{.items[0].metadata.name}")'>> startup.sh
+echo 'kubectl port-forward $POD_NAME --address 0.0.0.0 1230:3000 > /dev/null 2>&1 &'>> startup.sh
+echo 'cat ~/kubegoat'>> startup.sh
+echo 'echo ==================================='>> startup.sh
+echo 'echo Hello and welcome to this scenario!'>> startup.sh
+echo 'echo ==================================='>> startup.sh
+
 echo done > /tmp/background0
--- scenario01/setup_msg.sh
+++ scenario01/setup_msg.sh
@@ -1,3 +1,1 @@
-echo waiting for init-background-script to finish
-while [ ! -f /tmp/background0 ]; do sleep 1; done
-echo Hello and welcome to this scenario!
+bash startup.sh
