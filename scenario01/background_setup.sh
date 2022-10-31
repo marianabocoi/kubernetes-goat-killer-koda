d5033ef4ffbc5a54cf821e7bb4d882f071eeac7f
Mariana B. (she/her/they/them) <10142989+marianabocoi@users.noreply.github.com>
2022-10-31 18:50:52 +0100

    wait before start

--- scenario01/background_setup.sh
+++ scenario01/background_setup.sh
@@ -6,4 +6,5 @@ git clone https://github.com/madhuakula/kubernetes-goat.git
 
 cd kubernetes-goat
 bash setup-kubernetes-goat.sh
+sleep 3
 bash access-kubernetes-goat.sh
---
+++ scenario01/setup_msg.sh
@@ -0,0 +1,20 @@
+echo -n 'Setting up the cluster'
+sleep 1
+echo -n '.' 
+sleep 1
+echo -n '.' 
+sleep 1
+echo -n '.' 
+sleep 1
+echo -n '.' 
+sleep 1
+echo -n '.' 
+sleep 1
+echo -n '.' 
+sleep 1
+echo -n '.' 
+sleep 1
+echo '.' 
+sleep 1
+# Test for the setup to be actually up
+echo 'Done' 
