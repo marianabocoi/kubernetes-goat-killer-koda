20ce96edd961d1a48a342a09bd2f89e62e366fca
Mariana B. (she/her/they/them) <10142989+marianabocoi@users.noreply.github.com>
2022-10-31 23:07:12 +0100

    fix2

--- scenario01/background_setup.sh
+++ scenario01/background_setup.sh
@@ -20,9 +20,7 @@ echo "   __/ |                                             " >>kubegoat
 echo "  |___/  " >>kubegoat
 
 
-echo 'echo waiting for init-background-script to finish'> startup.sh
-echo 'while [ ! -f /tmp/background0 ]; do sleep 1; done'>> startup.sh
-echo 'export POD_NAME=$(kubectl get pods --namespace default -l "app=build-code" -o jsonpath="{.items[0].metadata.name}")'>> startup.sh
+echo 'export POD_NAME=$(kubectl get pods --namespace default -l "app=build-code" -o jsonpath="{.items[0].metadata.name}")'> startup.sh
 echo 'kubectl port-forward $POD_NAME --address 0.0.0.0 1230:3000 > /dev/null 2>&1 &'>> startup.sh
 echo 'cat ~/kubegoat'>> startup.sh
 echo 'echo ==================================='>> startup.sh
